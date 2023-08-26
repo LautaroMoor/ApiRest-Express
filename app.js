0888e1292bb451fdd97a59ab55fc6fcd3bd0ec40
LautaroMoor <lautimoor2@gmail.com>
2023-08-25 21:56:36 -0300

    Commit general

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config() //Ver esto
+// console.log(process.env) 
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
